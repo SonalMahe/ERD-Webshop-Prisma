07447682852bf1935c3a959479cd0b7b6bb3c58c
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-03-31 21:17:38 +0200

    check all endpoint on insomnia
    -work fine get,post,patch delete

--- index.ts
+++ index.ts
@@ -10,13 +10,21 @@ app.post("/products", async (req, res) => {
   try { 
     const newProduct = await prisma.product.create({ data: req.body }); 
     res.json(newProduct); 
-  } catch (error) { res.status(500).send(error instanceof Error ? error.message : "Unknown error"); } 
+  } catch (error) { 
+    res.status(500).send(error instanceof Error ? error.message : "Unknown error"); } 
 }); 
  
+
 // Get products with query filters (req.query) 
 // Examples: 
 // /products?category=Electronics 
 // /products?minPrice=10&maxPrice=100 
+
+app.get("/", (req, res) => {
+  res.send("API is working fine");
+});
+
+
 app.get("/products", async (req, res) => { 
   try { 
     const { category, minPrice, maxPrice } = req.query; 
@@ -34,15 +42,51 @@ app.get("/products", async (req, res) => {
   } catch (error) { res.status(500).send(error instanceof Error ? error.message : "Unknown error"); } 
 }); 
  
+
+
 // Update product by id (req.params) 
-app.patch("/products/:productId", async (req, res) => { 
-  try { 
-    const updated = await prisma.product.update({ 
-      where: { id: Number(req.params.productId) }, 
-      data: req.body 
-    }); 
-    res.json(updated); 
-  } catch (error) { res.status(500).send(error instanceof Error ? error.message : "Unknown error"); } 
-}); 
+app.patch("/products/:productId", async (req, res) => {
+  try {
+    const id = Number(req.params.productId);
+    const { name, price, stock, categoryId } = req.body;
+    const updated = await prisma.product.update({
+      where: { id },
+      data: {
+        name,
+        price,
+        stock,
+        categoryId,
+      },
+    });
+
+    res.json(updated);
+  } catch (error) {
+    res.status(500).send(error instanceof Error ? error.message : "Unknown error");
+  }
+});
  
-app.listen(3000, () => console.log("Server running on 3000")); 
+
+
+// Delete order by id (req.params)-
+app.delete("/orders/:orderId", async (req, res) => {
+  try {
+    const orderId = Number(req.params.orderId);
+
+    // delete related order items first (avoid FK error)
+    await prisma.orderItem.deleteMany({
+      where: { orderId },
+    });
+    await prisma.order.delete({
+      where: { id: orderId },
+    });
+
+    res.json({ message: "Order deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete order" });
+  }
+});
+
+
+app.listen(3000, () => {
+    console.log("Server running on 3000");
+}); 
